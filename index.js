600973b8b852e05921f303e0ddee0840a7888967
Himanshu Seth <hseth879157@gmail.com>
2026-01-03 16:06:20 +0530

    Add swwager documentaion.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 import productRoutes from "./src/features/product/routes/product.routes.js";
 import userRoutes from "./src/features/user/routes/user.routes.js";
 import cartRoutes from "./src/features/cart/routes/cart.route.js";
 import cookieParser from "cookie-parser";
+
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerDocument));
 app.use("/api/product", productRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/cart", cartRoutes);
